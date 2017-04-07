f4aecac507d6d54d1a45c761fb777468add05ed2
aiminy <jlfmssm@gmail.com>
2017-04-07 13:42:18 -0400

    correct a typo

--- inst/bin/rscript/analysisdog.r
+++ inst/bin/rscript/analysisdog.r
@@ -3,23 +3,26 @@ args = commandArgs(trailingOnly = TRUE)
 # test if there is at least one argument: if not, return an
 # error
 
-if (length(args) == 0) {
+if (length(args) == 0)
+{
     stop("At least one argument must be supplied (input file).n",
         call. = FALSE)
-} else if (length(args) > 1) {
-    input.count.file.dir = arg[1]
-    input.file.pattern = arg[2]
-    output.anlysis.dir = arg[3]
-    out.file.pattern.interested = arg[4]
-    out.file.pattern.positive.gene = arg[5]
-    out.file.pattern.negative.gene = arg[6]
-    out.file.pattern.all = arg[7]
-    dir.name.gene.list = arg[8]
-    pattern.4.gene.list = arg[9]
-    adjust_by_batch = arg[10]
+} else if (length(args) > 1)
+{
+    input.count.file.dir = args[1]
+    input.file.pattern = args[2]
+    output.anlysis.dir = args[3]
+    out.file.pattern.interested = args[4]
+    out.file.pattern.positive.gene = args[5]
+    out.file.pattern.negative.gene = args[6]
+    out.file.pattern.all = args[7]
+    dir.name.gene.list = args[8]
+    pattern.4.gene.list = args[9]
+    adjust_by_batch = args[10]
 }
 
-if (!dir.exists(output.count.file.dir)) {
+if (!dir.exists(output.count.file.dir))
+{
     dir.create(output.count.file.dir)
 }
 
@@ -30,4 +33,4 @@ Re.unadjusted.adjusted <- ProcessOutputFilesFromDoGsOnly(input.count.file.dir,
     input.file.pattern, output.anlysis.dir, out.file.pattern.interested,
     out.file.pattern.positive.gene, out.file.pattern.negative.gene,
     out.file.pattern.all, dir.name.gene.list, pattern.4.gene.list,
-    adjust_by_batch = "YES")
+    adjust_by_batch)
