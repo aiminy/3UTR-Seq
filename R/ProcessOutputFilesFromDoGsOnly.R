dff07e7f69dc44cd9f5540654ae20b8a86ab3131
aiminy <jlfmssm@gmail.com>
2017-04-11 19:33:28 -0400

    reorganize code

--- R/ProcessOutputFilesFromDoGsOnly.R
+++ R/ProcessOutputFilesFromDoGsOnly.R
@@ -1,3 +1,195 @@
+DEAnalysis <- function(countData,wt.index,dox.index)
+{
+
+  a <- length(wt.index)
+  b <- length(dox.index)
+
+  colData <- data.frame(condition = factor(c(rep("Dox",
+                                                 b), rep("WT", a))))
+
+  dds <- DESeqDataSetFromMatrix(countData, colData, formula(~condition))
+
+  # size.factor<-estimateSizeFactors(dds)
+
+  # print(size.factor)
+
+  re.DESeq <- results(DESeq(dds))
+
+  re.FC <- cbind(as.data.frame(re.DESeq), 2^re.DESeq[,
+                                                     2], counts(dds))
+  colnames(re.FC)[7] = "FoldChange(WT/Dox)"
+
+  txs.gene <- ReformatTxsGene()
+
+  re.FC <- merge(re.FC, txs.gene$txs_genes_DF_2, by = 0)
+
+  re.FC.sorted <- re.FC[order(re.FC$pvalue), ]
+
+  return(re.FC.sorted)
+
+}
+
+getutrcount <- function(dir.name, input.file.pattern)
+{
+  file.name = file.path(dir.name, dir(dir.name, recursive = TRUE,
+                                      pattern = input.file.pattern))
+  file.name.2 <- as.list(file.name)
+
+  names(file.name.2) = basename(file.name)
+
+  re.out <- lapply(file.name.2, function(u)
+  {
+    re = read.table(u, header = F)
+    colnames(re) = c("Count", "GeneName")
+    re
+  })
+
+  temp.name <- strsplit(names(file.name.2), split = "\\.")
+  temp.name.2 <- trimws(do.call("rbind", lapply(temp.name,
+                                                "[[", 1)))
+
+  temp.name.3 <- unique(temp.name.2[, 1])
+
+  temp.name.4 <- as.list(temp.name.3)
+  names(temp.name.4) <- temp.name.3
+
+  txs.name <- lapply(re.out, "[[", 2)
+  txs.name <- unlist(txs.name)
+  txs.name <- unique(as.character(txs.name))
+  txs.name.count <- data.frame(Count = rep(0, length(txs.name)),
+                               GeneName = txs.name)
+
+  # dim(txs.name.count)
+
+  Process4EachSample <- function(one_sample, txs.name.count,
+                                 re.out)
+  {
+
+    count.gene.matched <- re.out[(grep(one_sample, names(re.out)))]
+
+    count.gene.matched.minus.gene <- count.gene.matched[(grep("minus.gene",
+                                                              names(count.gene.matched)))]
+    count.gene.matched.plus.gene <- count.gene.matched[(grep("plus.gene",
+                                                             names(count.gene.matched)))]
+
+    ReformatCount <- function(count.gene.matched.minus.gene,
+                              txs.name.count)
+    {
+
+      reformat.count.gene.matched <- lapply(count.gene.matched.minus.gene,
+                                            function(x, txs.name.count)
+                                            {
+                                              y <- txs.name.count
+                                              if (dim(x[which(x$GeneName %in% y$GeneName),
+                                                        ])[1] != 0)
+                                              {
+                                                y[match(x$GeneName, y$GeneName), ]$Count <- x$Count
+                                              }
+                                              y
+                                            }, txs.name.count)
+
+
+      reformat.count.gene.matched.2 <- cbind(apply(do.call("cbind",
+                                                           lapply(reformat.count.gene.matched, "[", 2)),
+                                                   1, unique), do.call("cbind", lapply(reformat.count.gene.matched,
+                                                                                       "[", 1)))
+
+      colnames(reformat.count.gene.matched.2) = c("GeneName",
+                                                  names(count.gene.matched.minus.gene))
+
+      gene <- as.character(reformat.count.gene.matched.2$GeneName)
+
+      count.DoGs.plus.read <- apply(as.data.frame(reformat.count.gene.matched.2[,
+                                                                                c(grep("plus.read.DoGs.count.txt", colnames(reformat.count.gene.matched.2)))]),
+                                    1, sum)
+      count.DoGs.minus.read <- apply(as.data.frame(reformat.count.gene.matched.2[,
+                                                                                 c(grep("minus.read.DoGs.count.txt", colnames(reformat.count.gene.matched.2)))]),
+                                     1, sum)
+
+
+      reformat.count.gene.matched.3 <- as.data.frame(cbind(gene,
+                                                           count.DoGs.plus.read, count.DoGs.minus.read))
+
+      return(reformat.count.gene.matched.3)
+    }
+
+    re1 <- ReformatCount(count.gene.matched.minus.gene, txs.name.count)
+    re2 <- ReformatCount(count.gene.matched.plus.gene, txs.name.count)
+    re <- rbind(re1, re2)
+
+    re <- re[order(re$gene), ]
+
+    return(re)
+  }
+
+  re.8.samples <- lapply(temp.name.4, function(u, txs.name.count,
+                                               re.out)
+  {
+    YY <- Process4EachSample(u, txs.name.count, re.out)
+    YY
+  }, txs.name.count, re.out)
+
+  ProcessEachCorner <- function(re.8.samples, i)
+  {
+
+    gene <- apply(do.call("cbind", lapply(re.8.samples, "[",
+                                          1)), 1, unique)
+    count.gene.plus.read.8.samples <- cbind(gene, do.call("cbind",
+                                                          lapply(re.8.samples, "[", i)))
+
+    colnames(count.gene.plus.read.8.samples) = c("gene",
+                                                 names(re.8.samples))
+
+    df <- count.gene.plus.read.8.samples
+    df <- apply(df[, -1], 2, as.numeric)
+    index <- rowSums(df[, -1]) > 0
+    dff <- count.gene.plus.read.8.samples[index, ]
+    rownames(dff) <- dff$gene
+    dff <- dff[, -1]
+
+    wt.index <- grep("WT",toupper(colnames(dff)))
+
+    dox.index <- grep("DOX",toupper(colnames(dff)))
+
+    real.index <- c(dox.index,wt.index)
+
+    n.sample <- length(real.index)
+
+    permutation.index <- real.index
+
+    countData <- apply(dff[, permutation.index], 2, as.numeric)
+
+    rownames(countData) <- rownames(dff)
+
+    re.FC <- countData
+
+    return(re.FC)
+
+  }
+
+   re.BR <- ProcessEachCorner(re.8.samples, 2)  #BR
+   re.TR <- ProcessEachCorner(re.8.samples, 3)  #TR
+
+
+   re.BR.4.plus.gene.BL.4.minus.gene <- re.BR
+   re.TR.4.plus.gene.TL.4.minus.gene <- re.TR
+
+  # Get the counts for DoGs of plus and minus gene
+  DoGs.4.plus.Gene <- re.TR.4.plus.gene.TL.4.minus.gene[which(re.TR.4.plus.gene.TL.4.minus.gene$strand ==
+                                                                "+"), ]
+  DoGs.4.minus.Gene <- re.BR.4.plus.gene.BL.4.minus.gene[which(re.BR.4.plus.gene.BL.4.minus.gene$strand ==
+                                                                 "-"), ]
+
+  DoGs.4.plus.minus.Gene <- rbind(DoGs.4.plus.Gene, DoGs.4.minus.Gene)
+
+  res <- list(DoGs.4.plus.Gene=DoGs.4.minus.Gene,
+              DoGs.4.minus.Gene=DoGs.4.minus.Gene,
+              DoGs.4.plus.minus.Gene=DoGs.4.plus.minus.Gene)
+
+  return(res)
+
+}
+
 #' ProcessOutputFilesFromDoGsOnly
 #'
 #' @param dir.name
@@ -55,257 +247,12 @@ ProcessOutputFilesFromDoGsOnly <- function(dir.name, input.file.pattern,
     out.file.pattern.negative.gene, out.file.pattern.all, dir.name.gene.list,
     pattern.4.gene.list, adjust_by_batch,permutation.set.up=NULL)
     {
-    file.name = file.path(dir.name, dir(dir.name, recursive = TRUE,
-        pattern = input.file.pattern))
 
-    print(file.name)
+    x <- getutrcount(dir.name, input.file.pattern)
 
-    file.name.2 <- as.list(file.name)
-
-    names(file.name.2) = basename(file.name)
-
-    print(file.name.2)
-
-    re.out <- lapply(file.name.2, function(u)
-    {
-        re = read.table(u, header = F)
-        colnames(re) = c("Count", "GeneName")
-        re
-    })
-
-    temp.name <- strsplit(names(file.name.2), split = "\\.")
-
-    temp.name.2 <- trimws(do.call("rbind", lapply(temp.name,
-        "[[", 1)))
-
-    temp.name.3 <- unique(temp.name.2[, 1])
-
-    temp.name.4 <- as.list(temp.name.3)
-    names(temp.name.4) <- temp.name.3
-
-    txs.name <- lapply(re.out, "[[", 2)
-    txs.name <- unlist(txs.name)
-    txs.name <- unique(as.character(txs.name))
-    txs.name.count <- data.frame(Count = rep(0, length(txs.name)),
-        GeneName = txs.name)
-
-    # dim(txs.name.count)
-
-    Process4EachSample <- function(one_sample, txs.name.count,
-        re.out)
-        {
-
-        count.gene.matched <- re.out[(grep(one_sample, names(re.out)))]
-
-        count.gene.matched.minus.gene <- count.gene.matched[(grep("minus.gene",
-            names(count.gene.matched)))]
-        count.gene.matched.plus.gene <- count.gene.matched[(grep("plus.gene",
-            names(count.gene.matched)))]
-
-        ReformatCount <- function(count.gene.matched.minus.gene,
-            txs.name.count)
-            {
-
-            reformat.count.gene.matched <- lapply(count.gene.matched.minus.gene,
-                function(x, txs.name.count)
-                {
-                  y <- txs.name.count
-                  if (dim(x[which(x$GeneName %in% y$GeneName),
-                    ])[1] != 0)
-                    {
-                    y[match(x$GeneName, y$GeneName), ]$Count <- x$Count
-                  }
-                  y
-                }, txs.name.count)
-
-
-            reformat.count.gene.matched.2 <- cbind(apply(do.call("cbind",
-                lapply(reformat.count.gene.matched, "[", 2)),
-                1, unique), do.call("cbind", lapply(reformat.count.gene.matched,
-                "[", 1)))
-
-            colnames(reformat.count.gene.matched.2) = c("GeneName",
-                names(count.gene.matched.minus.gene))
-
-            gene <- as.character(reformat.count.gene.matched.2$GeneName)
-
-            count.DoGs.plus.read <- apply(as.data.frame(reformat.count.gene.matched.2[,
-                c(grep("plus.read.DoGs.count.txt", colnames(reformat.count.gene.matched.2)))]),
-                1, sum)
-            count.DoGs.minus.read <- apply(as.data.frame(reformat.count.gene.matched.2[,
-                c(grep("minus.read.DoGs.count.txt", colnames(reformat.count.gene.matched.2)))]),
-                1, sum)
-
-
-            reformat.count.gene.matched.3 <- as.data.frame(cbind(gene,
-                count.DoGs.plus.read, count.DoGs.minus.read))
-
-            return(reformat.count.gene.matched.3)
-        }
-
-        re1 <- ReformatCount(count.gene.matched.minus.gene, txs.name.count)
-        re2 <- ReformatCount(count.gene.matched.plus.gene, txs.name.count)
-        re <- rbind(re1, re2)
-
-        re <- re[order(re$gene), ]
-
-        return(re)
-    }
-
-    re.8.samples <- lapply(temp.name.4, function(u, txs.name.count,
-        re.out)
-        {
-        YY <- Process4EachSample(u, txs.name.count, re.out)
-        YY
-    }, txs.name.count, re.out)
-
-    DEAnalysis <- function(countData,wt.index,dox.index)
-    {
-
-        a <- length(wt.index)
-        b <- length(dox.index)
-
-        colData <- data.frame(condition = factor(c(rep("Dox",
-            b), rep("WT", a))))
-
-        dds <- DESeqDataSetFromMatrix(countData, colData, formula(~condition))
-
-        # size.factor<-estimateSizeFactors(dds)
-
-        # print(size.factor)
-
-        re.DESeq <- results(DESeq(dds))
-
-        re.FC <- cbind(as.data.frame(re.DESeq), 2^re.DESeq[,
-            2], counts(dds))
-        colnames(re.FC)[7] = "FoldChange(WT/Dox)"
-
-        txs.gene <- ReformatTxsGene()
-
-        re.FC <- merge(re.FC, txs.gene$txs_genes_DF_2, by = 0)
-
-        re.FC.sorted <- re.FC[order(re.FC$pvalue), ]
-
-        return(re.FC.sorted)
-
-    }
-
-    ProcessEachCorner <- function(re.8.samples, i)
-    {
-
-        gene <- apply(do.call("cbind", lapply(re.8.samples, "[",
-            1)), 1, unique)
-        count.gene.plus.read.8.samples <- cbind(gene, do.call("cbind",
-            lapply(re.8.samples, "[", i)))
-
-        colnames(count.gene.plus.read.8.samples) = c("gene",
-            names(re.8.samples))
-
-        df <- count.gene.plus.read.8.samples
-        df <- apply(df[, -1], 2, as.numeric)
-        index <- rowSums(df[, -1]) > 0
-        dff <- count.gene.plus.read.8.samples[index, ]
-        rownames(dff) <- dff$gene
-        dff <- dff[, -1]
-
-        # print(head(dff))
-
-        # countData <- apply(dff[,c(1,3,5,6,2,4,7,8)], 2, as.numeric)
-
-        wt.index <- grep("WT",toupper(colnames(dff)))
-
-        dox.index <- grep("DOX",toupper(colnames(dff)))
-
-        real.index <- c(dox.index,wt.index)
-
-        #real.index <- c(1, 3, 4, 2, 5, 6)
-
-        n.sample <- length(real.index)
-
-        permutation.index <- real.index
-        # permutation.index=array(sample(real.index))
-
-        countData <- apply(dff[, permutation.index], 2, as.numeric)
-
-        rownames(countData) <- rownames(dff)
-
-        #re.FC <- DEAnalysis(countData,wt.index,dox.index)
-
-        # re.DoGs.adjusted.by.batch<-DEAnalysisAdjustByBatch(countData)
-
-        # re.FC<-cbind(as.data.frame(re.DESeq),2^re.DESeq[,2],counts(dds))
-        # colnames(re.FC)[7]='FoldChange(WT/Dox)'
-        # txs.gene<-ReformatTxsGene()
-        # re.FC<-list(re.FC=re.FC,re.DoGs.adjusted.by.batch=re.DoGs.adjusted.by.batch)
-
-        #re.FC<-list(re.FC=re.FC,n=n.sample)
-
-        re.FC <- countData
-
-        return(re.FC)
-
-    }
-
-    # re.BL<-ProcessEachCorner(re.8.samples,2) #BL
-    # re.TL<-ProcessEachCorner(re.8.samples,3) #TL
-
-    #re.BR <- ProcessEachCorner(re.8.samples, 2)  #BR
-    #re.TR <- ProcessEachCorner(re.8.samples, 3)  #TR
-
-    #n <- re.BR$n
-
-    # head(re.BL) head(re.BR)
-
-    # head(re.TL) head(re.TR)
-
-    # re.TR[which(re.TR$Row.names =='uc003tzi.4'),]
-
-    # Check for uc003tzi.4:gene(+)
-    # re.BL[[1]][which(re.BL[[1]]$Row.names =='uc003tzi.4'),] #BL
-    # for gene(+) re.TL[[1]][which(re.TL[[1]]$Row.names
-    # =='uc003tzi.4'),] #TL for gene(+) gene
-    # re.BR[[1]][which(re.BR[[1]]$Row.names =='uc003tzi.4'),] #BR
-    # for gene(+) re.TR[[1]][which(re.TR[[1]]$Row.names
-    # =='uc003tzi.4'),] #TR for gene(+) DoGs
-
-    # Check for uc002szf.1:gene(-)
-    # re.BL[[1]][which(re.BL[[1]]$Row.names =='uc002szf.1'),] #BR
-    # for gene(-) re.TL[[1]][which(re.TL[[1]]$Row.names
-    # =='uc002szf.1'),] # re.BR[[1]][which(re.BR[[1]]$Row.names
-    # =='uc002szf.1'),] # re.TR[[1]][which(re.TR[[1]]$Row.names
-    # =='uc002szf.1'),] #
-
-    # Check for uc001aac.4:gene(-)
-    # re.BL[[1]][which(re.BL[[1]]$Row.names =='uc001aac.4'),]#BR
-    # for gene(-) Gene re.TL[[1]][which(re.TL[[1]]$Row.names
-    # =='uc001aac.4'),]#TR for gene(-)
-    # re.TR[[1]][which(re.BR[[1]]$Row.names =='uc001aac.4'),]#TL
-    # for gene(-) re.BR[[1]][which(re.TR[[1]]$Row.names
-    # =='uc001aac.4'),]#BL for gene(-) DoGs
-
-    # if(adjust_by_batch=='NO'){
-    # re.BL.4.plus.gene.BR.4.minus.gene<-re.BL
-    # re.TL.4.plus.gene.TR.4.minus.gene<-re.TL
-
-    re.BR.4.plus.gene.BL.4.minus.gene <- re.BR
-    re.TR.4.plus.gene.TL.4.minus.gene <- re.TR
-    # }else{ re.BL.4.plus.gene.BR.4.minus.gene<-re.BL[[2]]
-    # re.TL.4.plus.gene.TR.4.minus.gene<-re.TL[[2]]
-    # re.BR.4.plus.gene.BL.4.minus.gene<-re.BR[[2]]
-    # re.TR.4.plus.gene.TL.4.minus.gene<-re.TR[[2]] }
-
-    # Get the counts for DoGs of plus and minus gene
-    DoGs.4.plus.Gene <- re.TR.4.plus.gene.TL.4.minus.gene[which(re.TR.4.plus.gene.TL.4.minus.gene$strand ==
-        "+"), ]
-    DoGs.4.minus.Gene <- re.BR.4.plus.gene.BL.4.minus.gene[which(re.BR.4.plus.gene.BL.4.minus.gene$strand ==
-        "-"), ]
-
-    DoGs.4.plus.minus.Gene <- rbind(DoGs.4.plus.Gene, DoGs.4.minus.Gene)
-
-    # DoGs.4.plus.Gene[which(DoGs.4.plus.Gene$Row.names
-    # =='uc003tzi.4'),] #TR for gene(+) DoGs
-    # DoGs.4.minus.Gene[which( DoGs.4.minus.Gene$Row.names
-    # =='uc001aac.4'),]#BL for gene(-) DoGs
+    DoGs.4.plus.Gene <- x[[1]]
+    DoGs.4.minus.Gene <- x[[2]]
+    DoGs.4.plus.minus.Gene <- x[[3]]
 
     GeneTypeBasedDE <- function(DoGs.4.plus.Gene)
     {
@@ -411,7 +358,7 @@ ProcessOutputFilesFromDoGsOnly <- function(dir.name, input.file.pattern,
     # = FALSE,quote=FALSE)
 
 
-    re.out.3 <- list(re.out = re.out, re.8.samples = re.8.samples,
+    re.out.3 <- list(dogcount=x,
         DE_positive_gene = re.DESeq.DoGs.plus.gene, DE_negative_gene = re.DESeq.DoGs.minus.gene,
         DE = re.DESeq.DoGs.plus.minus.gene, DE_interested = re.DESeq.DoGs.plus.minus.gene.interested)
 
