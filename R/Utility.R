7f9517757786ba32bacae0d0ff183c4f6d2c7384
aiminy <jlfmssm@gmail.com>
2017-05-17 04:01:07 -0400

    change

--- R/Utility.R
+++ R/Utility.R
@@ -419,10 +419,10 @@ getcountsfromMatchedbed <- function(input.bedfile.dir, output.count.file.dir,
     m.id <- grep("login", system("hostname", intern = TRUE))
     cmd0 <- "awk -F '\\t'"
 
-    cmd1 <-  '\\$6==\"+\" && \$12==\"-\"'
-    cmd11 <- '\\$6==\"+\" && \$12==\"+\"'
-    cmd12 <- '\\$6==\"-\" && \$12==\"+\"'
-    cmd13 <- '\\$6==\"-\" && \$12==\"-\"'
+    cmd1 <-  '\\$6==\"+\" && \\$12==\"-\"'
+    cmd11 <- '\\$6==\"+\" && \\$12==\"+\"'
+    cmd12 <- '\\$6==\"-\" && \\$12==\"+\"'
+    cmd13 <- '\\$6==\"-\" && \\$12==\"-\"'
 
     cmd2 <- "| awk '\\$8 < \\$2 && \\$9 >= \\$2' | awk '{print \\$4}' | sort | uniq -c | sort -nr"  #below
     cmd21 <- "| awk '\\$8 >= \\$2 && \\$9 <= \\$3' | awk '{print \\$4}' | sort | uniq -c | sort -nr"  #DoGs
