734020594f6c433b2cf333167e64c695e2c509d8
aiminy <jlfmssm@gmail.com>
2017-05-21 22:35:27 -0400

    re test

--- R/runDoGs.R
+++ R/runDoGs.R
@@ -9,7 +9,7 @@ runDoGs <- function(sra.accession.number,sample.info.file,gene.gtf,genome.index,
 
 re <- ThreeUTR:::useWget2Download(sra.accession.number,file.path(output.dir,"SRAFiles"))
 
-Rfun1 <- 'library(ChipSeq);library(ThreeUTR);re <- ThreeUTR:::useFastqDumpConvertSra2Fastq('
+Rfun1 <- "library(ChipSeq);library(ThreeUTR);re <- ThreeUTR:::useFastqDumpConvertSra2Fastq("
 
 input=file.path(output.dir,"SRAFiles")
 output=file.path(output.dir,"Fastqfiles")
